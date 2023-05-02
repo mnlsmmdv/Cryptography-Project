648664265393db406cbed1d23b1a140e68d96989
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-02 21:53:54 +0500

    File description set

---
+++ task_one.py
@@ -0,0 +1,12 @@
+"""
+Name: Ahmed Affaan
+Title: task_one.py
+Folder: Cryptography-Project
+Date: 02/05/2023
+Country: Republic of Maldives
+Code version: -
+Description: -
+Note: Uncomment codes to execute and comment them when not in use.
+"""
+
+
